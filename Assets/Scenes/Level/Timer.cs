b66bc819d8de1c2081a507e2289af8763b565ef0
iqbaltahir33 <iqbalknightphantom33@gmail.com>
2022-02-13 16:19:46 +0800

    first commit

---
+++ Assets/Scenes/Level/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Slider timerSlider;
+    public Text timerText;
+    public float gameTime;
+
+    private bool stopTimer;
+
+    void Start()
+    {
+        stopTimer = false;
+        timerSlider.maxValue = gameTime;
+        timerSlider.value = gameTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = gameTime-Time.time;
+
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time - minutes * 60f);
+
+        string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        if (time <= 0)
+        {
+            stopTimer = true;
+        }
+        if (stopTimer == false)
+        {
+            timerText.text = textTime;
+            timerSlider.value = time;
+            
+        }
+    }
+}
---
+++ Assets/script/PindahScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PindahScene : MonoBehaviour
+{
+    public void scene(string scene){
+        Application.LoadLevel(scene);
+    }
+}
